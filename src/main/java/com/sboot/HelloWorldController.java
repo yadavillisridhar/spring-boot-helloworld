971e199706a6bc138d58a937cea9e1de95d30918
Sridhar Yadavilli <yadavillisridhar@gmail.com>
2024-05-02 00:59:48 +0530

    Initial commit

---
+++ src/main/java/com/sboot/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.sboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloWorldController {
+
+  @GetMapping(value = "hello")
+  public String helloWorld() {
+    return "Hello World!";
+  }
+
+}
